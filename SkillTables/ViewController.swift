0bfc72d1606cc58f40c448b0e399b9305c551990
adeveloper2015 <hudiakov@gmail.com>
2017-03-30 19:04:13 +0300

    first

---
+++ SkillTables/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  SkillTables
+//
+//  Created by adeveloper on 30.03.17.
+//  Copyright © 2017 adeveloper. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
