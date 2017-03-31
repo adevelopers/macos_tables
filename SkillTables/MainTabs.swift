682d368551534a3dca1b7f11e7682d89aeb988e7
adeveloper2015 <hudiakov@gmail.com>
2017-03-31 14:18:26 +0300

    mainTab added

---
+++ SkillTables/MainTabs.swift
@@ -0,0 +1,21 @@
+//
+//  MainTabs.swift
+//  SkillTables
+//
+//  Created by adeveloper on 30.03.17.
+//  Copyright © 2017 adeveloper. All rights reserved.
+//
+
+import Cocoa
+
+class MainTabs: NSTabViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+    @IBAction func menuQuit(_ sender: Any) {
+        exit(0)
+    }
+}
