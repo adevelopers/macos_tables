48886ca6937ab03a202a34fae675b764458c253d
adeveloper2015 <hudiakov@gmail.com>
2017-04-03 01:02:34 +0300

    AboutApp

---
+++ SkillTables/AboutApp.swift
@@ -0,0 +1,18 @@
+//
+//  AboutApp.swift
+//  SkillTables
+//
+//  Created by adeveloper on 03.04.17.
+//  Copyright © 2017 adeveloper. All rights reserved.
+//
+
+import Cocoa
+
+class AboutApp: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+}
